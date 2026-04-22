d3b2731574dece453527c73caecba0a040f828b5
Bruno Murek <44498326+asprtam@users.noreply.github.com>
2026-04-22 15:48:56 +0200

    footer fix

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,4 +1,5 @@
 <footer class="footer">
     <p>Check the repo <a href="https://github.com/asprtam/clip-shape-creator">https://github.com/asprtam/clip-shape-creator</a></p>
+    <p>Inspired by <a href="https://bennettfeely.com/clippy/">https://bennettfeely.com/clippy/</a></p>
     <p>Menlo font taken from <a href="http://www.onlinewebfonts.com/fonts">Web Fonts</a> is licensed by CC BY 4.0</p>
 </footer>
